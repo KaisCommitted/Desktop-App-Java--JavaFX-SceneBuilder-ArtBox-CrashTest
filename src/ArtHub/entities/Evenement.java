64f335fdbe93b4ed4aec881c57ae927bb74ed580
Kais <kais.lamine@esprit.tn>
2021-03-02 21:43:08 +0100

    Update Evenement.java

--- src/ArtHub/entities/Evenement.java
+++ src/ArtHub/entities/Evenement.java
@@ -6,7 +6,7 @@ import java.io.Serializable;
 import java.sql.Date;
 
 /**
- *
+ *test
  * @author Fayechi
  */
 public class Evenement  extends RecursiveTreeObject<Evenement> implements Serializable {
