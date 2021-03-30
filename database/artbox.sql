7b85d354fd4d4517a00a21ecc2b34a02492b553a
Kais <kais.lamine@esprit.tn>
2021-03-30 22:54:11 +0100

    Update artbox.sql

--- database/artbox.sql
+++ database/artbox.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 30, 2021 at 11:31 PM
+-- Generation Time: Mar 30, 2021 at 11:53 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.4.15
 
