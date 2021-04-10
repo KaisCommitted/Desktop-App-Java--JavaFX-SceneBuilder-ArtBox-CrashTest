288e2a0c85ca07d6a799740bf77189768e5d1152
Kais <kais.lamine@esprit.tn>
2021-04-11 00:48:27 +0100

    last

--- src/ArtHub/gui/MyListener.java
+++
@@ -1,7 +0,0 @@
-package ArtHub.gui;
-
-import ArtHub.entities.Post;
-
-public interface MyListener {
-    public void onClickListener(Post post);
-}
--- src/SentimentAnalysis/Sentence.java
+++
@@ -1,8 +0,0 @@
-package SentimentAnalysis;
-public class Sentence{
-    public String sentiment;
-    public ConfidenceScores confidenceScores;
-    public int offset;
-    public int length;
-    public String text;
-}
